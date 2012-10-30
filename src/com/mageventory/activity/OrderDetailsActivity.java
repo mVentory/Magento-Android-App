fa7478d71717b51980eefa2e020eefc6999e52e0
possitron <possitron@805d05ed-e12b-7278-cd1c-88623764219b>
2012-10-30 17:41:29 +0000

    #290
    git-svn-id: http://mageventory.googlecode.com/svn/trunk@887 805d05ed-e12b-7278-cd1c-88623764219b

--- src/com/mageventory/activity/OrderDetailsActivity.java
+++ src/com/mageventory/activity/OrderDetailsActivity.java
@@ -1,5 +1,11 @@
 package com.mageventory.activity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Map;
 
 import com.mageventory.R;
@@ -21,6 +27,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
@@ -40,8 +47,17 @@ public class OrderDetailsActivity extends BaseActivity {
 	private String mOrderIncrementId;
 	private Settings mSettings;
 	private LinearLayout mRawDumpLayout;
+	private Button mRawDumpButtonShow;
+	private Button mRawDumpButtonHide;
 	
 	private static final int INDENTATION_LEVEL_DP = 20;
+	
+	class Comment
+	{
+		public Date createdAt;
+		public String comment;
+		public String customerNotified;
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +88,27 @@ public class OrderDetailsActivity extends BaseActivity {
 		mLoadOrderDetailsDataTask.execute();
 		
 		mSettings = new Settings(this);
+		
+		mRawDumpButtonShow = (Button) findViewById(R.id.raw_dump_button_show);
+		mRawDumpButtonHide = (Button) findViewById(R.id.raw_dump_button_hide);
+		
+		mRawDumpButtonShow.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mRawDumpLayout.setVisibility(View.VISIBLE);
+				mRawDumpButtonShow.setEnabled(false);
+				mRawDumpButtonHide.setEnabled(true);
+			}
+		});
+		
+		mRawDumpButtonHide.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mRawDumpLayout.setVisibility(View.GONE);
+				mRawDumpButtonShow.setEnabled(true);
+				mRawDumpButtonHide.setEnabled(false);
+			}
+		});
 	}
 
 	/* Shows a dialog for adding new option. */
@@ -192,6 +229,373 @@ public class OrderDetailsActivity extends BaseActivity {
 		}
 	}
 	
+	
+	
+	private void rawDumpMapIntoLayout2(Map<String, Object> map, int nestingLevel, ArrayList<String> keys_to_show)
+	{
+		Resources r = getResources();
+		float indentationWidthPix = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, INDENTATION_LEVEL_DP * nestingLevel, r.getDisplayMetrics());
+		float arrayIndentationWidthPix = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, INDENTATION_LEVEL_DP * (nestingLevel+1), r.getDisplayMetrics());
+		
+		for (String key : map.keySet())
+		{
+			if (keys_to_show.contains(key) && map.get(key) instanceof String)
+			{
+				LinearLayout subitem = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+				
+				View indentation = subitem.findViewById(R.id.indentation);
+				
+				indentation.setLayoutParams(new LinearLayout.LayoutParams((int)indentationWidthPix, 0));
+				
+				TextView text1 = (TextView)subitem.findViewById(R.id.text1);
+				TextView text2 = (TextView)subitem.findViewById(R.id.text2);
+				
+				text1.setText(key + ": ");
+				text2.setText((String)map.get(key));
+				mRawDumpLayout.addView(subitem);
+			}
+		}
+
+		for (String key : map.keySet())
+		{
+			if (keys_to_show.contains(key) && map.get(key) instanceof Map)
+			{
+				LinearLayout subitem = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+				
+				View indentation = subitem.findViewById(R.id.indentation);
+				
+				indentation.setLayoutParams(new LinearLayout.LayoutParams((int)indentationWidthPix, 0));
+				
+				TextView text1 = (TextView)subitem.findViewById(R.id.text1);
+				text1.setText(key);
+				mRawDumpLayout.addView(subitem);
+				
+				rawDumpMapIntoLayout((Map<String, Object>)map.get(key), nestingLevel + 1);
+			}
+		}
+				
+		for (String key : map.keySet())
+		{
+			if (keys_to_show.contains(key) && map.get(key) instanceof Object[])
+			{
+				LinearLayout subitem = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+				
+				View indentation = subitem.findViewById(R.id.indentation);
+				
+				indentation.setLayoutParams(new LinearLayout.LayoutParams((int)indentationWidthPix, 0));
+				
+				TextView text1 = (TextView)subitem.findViewById(R.id.text1);
+				text1.setText(key);
+				mRawDumpLayout.addView(subitem);
+				
+				for(int i=0; i<((Object [])map.get(key)).length; i++)
+				{
+					LinearLayout arraySubitem = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+					View arraySubitemIndentation = arraySubitem.findViewById(R.id.indentation);
+					
+					arraySubitemIndentation.setLayoutParams(new LinearLayout.LayoutParams((int)arrayIndentationWidthPix, 0));
+					
+					TextView arrayItemText1 = (TextView)arraySubitem.findViewById(R.id.text1);
+					arrayItemText1.setText(key + "[" + i + "]");
+					mRawDumpLayout.addView(arraySubitem);
+					
+					rawDumpMapIntoLayout2((Map<String, Object>)(((Object[])map.get(key))[i]), nestingLevel + 2, keys_to_show);
+				}
+			}
+		}
+	}
+	
+	private void rawDumpMapIntoLayout3(String key, Object [] array, int nestingLevel, ArrayList<String> keys_to_show)
+	{
+		Resources r = getResources();
+		float indentationWidthPix = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, INDENTATION_LEVEL_DP * nestingLevel, r.getDisplayMetrics());
+		
+		for(int i=0; i<array.length; i++)
+		{
+			LinearLayout arraySubitem = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+			View arraySubitemIndentation = arraySubitem.findViewById(R.id.indentation);
+					
+			arraySubitemIndentation.setLayoutParams(new LinearLayout.LayoutParams((int)indentationWidthPix, 0));
+					
+			TextView arrayItemText1 = (TextView)arraySubitem.findViewById(R.id.text1);
+			arrayItemText1.setText(key + "[" + i + "]");
+			mRawDumpLayout.addView(arraySubitem);
+			
+			rawDumpMapIntoLayout2((Map<String, Object>)array[i], nestingLevel + 2, keys_to_show);
+		}
+	}
+	
+	private void createAccountInformationSection()
+	{
+		final String [] KEYS_TO_SHOW = {"customer_lastname", "customer_firstname", "customer_email"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Account information");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		rawDumpMapIntoLayout2(mLoadOrderDetailsDataTask.getData(), 1, keysToShowArrayList);
+	}
+	
+	private void createShippingAddressSection()
+	{
+		final String [] KEYS_TO_SHOW = {"email", "street", "country_id", "lastname", "firstname", "postcode", "telephone", "city"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Delivery Address");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("shipping_address") != null)
+		{
+			rawDumpMapIntoLayout2((Map<String, Object>)mLoadOrderDetailsDataTask.getData().get("shipping_address"), 1, keysToShowArrayList);	
+		}
+	}
+	
+	private void createPaymentSection()
+	{
+		final String [] KEYS_TO_SHOW = {"base_amount_ordered", "amount_paid", "method"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Payment");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("payment") != null)
+		{
+			rawDumpMapIntoLayout2((Map<String, Object>)mLoadOrderDetailsDataTask.getData().get("payment"), 1, keysToShowArrayList);	
+		}
+	}
+	
+	private void createCreditMemosSection()
+	{
+		final String [] KEYS_TO_SHOW = {"credit_memos", "increment_id", "created_at", "base_adjustment_negative", "base_shipping_amount",
+			"base_hidden_tax_amount", "base_subtotal_incl_tax", "base_discount_amount", "base_shipping_tax_amount", "base_shipping_incl_tax",
+			"base_subtotal", "base_adjustment_positive", "base_grand_total"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Credit Memos");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("credit_memos") != null)
+		{
+			rawDumpMapIntoLayout3("credit_memos", (Object[])mLoadOrderDetailsDataTask.getData().get("credit_memos"), 1, keysToShowArrayList);
+		}
+	}
+	
+	private void createStatusHistorySection()
+	{
+		final String [] KEYS_TO_SHOW = {"created_at", "entity_name", "status"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Status History");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("status_history") != null)
+		{
+			rawDumpMapIntoLayout3("status_history", (Object[])mLoadOrderDetailsDataTask.getData().get("status_history"), 1, keysToShowArrayList);
+		}
+	}
+	
+	private void createProductsListSection()
+	{
+		final String [] KEYS_TO_SHOW = {"base_price", "product_id", "qty_ordered", "name"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Products List");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("items") != null)
+		{
+			rawDumpMapIntoLayout3("items", (Object[])mLoadOrderDetailsDataTask.getData().get("items"), 1, keysToShowArrayList);
+		}
+	}
+	
+	private void createShipmentsSection()
+	{
+		final String [] KEYS_TO_SHOW = {"total_qty", "qty_ordered", "increment_id", "shipment_id", "items", "product_id", "name", "qty",
+			"tracks", "carrier_code", "title", "track_number"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Shipments");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("shipments") != null)
+		{
+			rawDumpMapIntoLayout3("shipments", (Object[])mLoadOrderDetailsDataTask.getData().get("shipments"), 1, keysToShowArrayList);
+		}
+	}
+	
+	private Comment parseComment(Map<String, Object> commentMap, boolean statusHistoryComment) throws ParseException
+	{
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		if (statusHistoryComment == false)
+		{
+			Comment comment = new Comment();
+			
+			comment.createdAt = dateFormatter.parse((String)commentMap.get("created_at")); 
+			comment.comment = (String) commentMap.get("comment");
+			comment.customerNotified = (String) commentMap.get("is_customer_notified");
+			
+			return comment;
+		}
+		
+		return null;
+	}
+
+	private void createCommentsSection()
+	{
+		final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		final String [] KEYS_TO_SHOW = {"created_at", "is_customer_notified", "comment"};
+		ArrayList<String> keysToShowArrayList = new ArrayList<String>();
+		Collections.addAll(keysToShowArrayList, KEYS_TO_SHOW);
+		
+		ArrayList<Object> commentsList = new ArrayList<Object>();
+		
+		LinearLayout header = (LinearLayout)mInflater.inflate(R.layout.order_details_sub_item, null);
+		TextView headerText = (TextView)header.findViewById(R.id.text1);
+		headerText.setText("Comments");
+		headerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		
+		mRawDumpLayout.addView(header);
+		
+		if (mLoadOrderDetailsDataTask.getData().get("credit_memos") != null)
+		{
+			Object[] creditMemos = (Object[])mLoadOrderDetailsDataTask.getData().get("credit_memos");
+			
+			for (Object cm : creditMemos)
+			{
+				Map<String, Object> creditMemo = (Map<String, Object>)cm;
+				
+				if (creditMemo.get("comments") != null)
+				{
+					for (Object comm : (Object[])creditMemo.get("comments"))
+					{
+						commentsList.add(comm);
+					}
+				}
+			}
+		}
+		
+		if (mLoadOrderDetailsDataTask.getData().get("invoices") != null)
+		{
+			Object[] invoices = (Object[])mLoadOrderDetailsDataTask.getData().get("invoices");
+			
+			for (Object inv : invoices)
+			{
+				Map<String, Object> invoice = (Map<String, Object>)inv;
+				
+				if (invoice.get("comments") != null)
+				{
+					for (Object comm : (Object[])invoice.get("comments"))
+					{
+						commentsList.add(comm);
+					}
+				}
+			}
+		}
+		
+		if (mLoadOrderDetailsDataTask.getData().get("shipments") != null)
+		{
+			Object[] shipments = (Object[])mLoadOrderDetailsDataTask.getData().get("shipments");
+			
+			for (Object shi : shipments)
+			{
+				Map<String, Object> shipment = (Map<String, Object>)shi;
+				
+				if (shipment.get("comments") != null)
+				{
+					for (Object comm : (Object[])shipment.get("comments"))
+					{
+						commentsList.add(comm);
+					}
+				}
+			}
+		}
+		
+		if (mLoadOrderDetailsDataTask.getData().get("status_history") != null)
+		{
+			Object[] statuses = (Object[])mLoadOrderDetailsDataTask.getData().get("status_history");
+			
+			for (Object st : statuses)
+			{
+				Map<String, Object> status = (Map<String, Object>)st;
+				
+				if (status.get("comment") != null)
+				{
+					commentsList.add(st);
+				}
+			}
+		}
+		
+		Collections.sort(commentsList, new Comparator<Object>() {
+
+			@Override
+			public int compare(Object lhs, Object rhs) {
+
+				Date leftDate = null;
+				Date rightDate = null;
+				
+				try {
+					leftDate = dateFormatter.parse((String)((Map<String, Object>)lhs).get("created_at"));
+					rightDate = dateFormatter.parse((String)((Map<String, Object>)rhs).get("created_at"));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} 
+				
+				if (leftDate == null || rightDate == null)
+					return 0;
+				
+				if (leftDate.after(rightDate))
+				{
+					return 1;
+				}
+				else
+				if (rightDate.after(leftDate))
+				{
+					return -1;
+				}
+				
+				return 0;
+			}
+		});
+		
+		rawDumpMapIntoLayout3("comments", commentsList.toArray(), 1, keysToShowArrayList);
+	}
+	
 	private void fillTextViewsWithData()
 	{
 		if (mLoadOrderDetailsDataTask == null || mLoadOrderDetailsDataTask.getData() == null)
@@ -212,7 +616,20 @@ public class OrderDetailsActivity extends BaseActivity {
 		
 		mRawDumpLayout.removeAllViews();
 		
-		rawDumpMapIntoLayout(mLoadOrderDetailsDataTask.getData(), 0);
+		////////////////
+		
+		createAccountInformationSection();
+		createShippingAddressSection();
+		createPaymentSection();
+		createCreditMemosSection();
+		createStatusHistorySection();
+		createProductsListSection();
+		createShipmentsSection();
+		createCommentsSection();
+		
+		////////////////
+		
+		//rawDumpMapIntoLayout(mLoadOrderDetailsDataTask.getData(), 0);
 	}
 	
 	@Override
