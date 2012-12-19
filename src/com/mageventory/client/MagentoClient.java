32819650a8e45603b5b7075fa245e5566ab8c3dc
possitron <possitron@805d05ed-e12b-7278-cd1c-88623764219b>
2012-12-19 08:47:08 +0000

    #172
    git-svn-id: http://mageventory.googlecode.com/svn/trunk@1012 805d05ed-e12b-7278-cd1c-88623764219b

--- src/com/mageventory/client/MagentoClient.java
+++ src/com/mageventory/client/MagentoClient.java
@@ -748,7 +748,7 @@ public class MagentoClient implements MageventoryConstants {
 					long transactionID = Long.valueOf(productData.get(MAGEKEY_PRODUCT_TRANSACTION_ID).toString()); 
 					String name = productData.get(MAGEKEY_PRODUCT_NAME).toString();
 					Object result = client.call("call", sessionId, "cart.createOrderForProduct", new Object[] { sku,
-							price, quantity, customerID, transactionID, name });
+							price, quantity, customerID, (int)(transactionID%(Integer.MAX_VALUE)), name });
 
 					if (result == null) {
 						return null;
