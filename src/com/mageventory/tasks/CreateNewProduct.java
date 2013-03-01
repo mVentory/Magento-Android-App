b22dcd97dd639c5f27fe9dd2f03a50dc8c25bc97
possitron <possitron@805d05ed-e12b-7278-cd1c-88623764219b>
2013-03-01 10:41:20 +0000

    #415, Making the phone generate SKUs prefixed with P instead of M.
    git-svn-id: http://mageventory.googlecode.com/svn/trunk@1361 805d05ed-e12b-7278-cd1c-88623764219b

--- src/com/mageventory/tasks/CreateNewProduct.java
+++ src/com/mageventory/tasks/CreateNewProduct.java
@@ -113,7 +113,7 @@ public class CreateNewProduct extends AsyncTask<Void, Void, Integer> implements 
 		/* Since we can't get microsecond time in java we just use milliseconds time and add microsecond part from System.nanoTime() which
 		 * doesn't return a normal timestamp but a number of nanoseconds from some arbitrary point in time which we don't know. This
 		 * should be enough to make every SKU we'll ever generate different. */
-		return "M" + System.currentTimeMillis() + (System.nanoTime()/1000)%1000;
+		return "P" + System.currentTimeMillis() + (System.nanoTime()/1000)%1000;
 	}
 	
 	@Override
