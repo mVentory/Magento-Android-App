5cf64aa8def0f3527161a37c691ace9c80ce2f31
Eugene Popovich <httpdispatch@gmail.com>
2014-04-02 11:19:40 +0300

    replaced � with — everywhere

--- mvApp/src/com/mageventory/activity/ProductDetailsActivity.java
+++ mvApp/src/com/mageventory/activity/ProductDetailsActivity.java
@@ -2,10 +2,10 @@
  * 
 * License       http://creativecommons.org/licenses/by-nc-nd/4.0/
 * 
-* NonCommercial � You may not use the material for commercial purposes. 
-* NoDerivatives � If you compile, transform, or build upon the material,
+* NonCommercial — You may not use the material for commercial purposes. 
+* NoDerivatives — If you compile, transform, or build upon the material,
 * you may not distribute the modified material. 
-* Attribution � You must give appropriate credit, provide a link to the license,
+* Attribution — You must give appropriate credit, provide a link to the license,
 * and indicate if changes were made. You may do so in any reasonable manner, 
 * but not in any way that suggests the licensor endorses you or your use. 
 */
