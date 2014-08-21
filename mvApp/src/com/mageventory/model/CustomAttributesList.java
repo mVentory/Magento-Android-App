c5b6829bef57ae54b39bb6e30efe819b9d35280d
Eugene Popovich <httpdispatch@gmail.com>
2014-08-21 13:21:40 +0300

    Task 1707 (Incorporate attribute metadata into search) https://rm.mventory.com/issues/1707
    - CustomAttributesList: removed temp default value 1 fix for the
    attribute parameter copyFromSearch from the createCustomAttribute method

--- mvApp/src/com/mageventory/model/CustomAttributesList.java
+++ mvApp/src/com/mageventory/model/CustomAttributesList.java
@@ -295,10 +295,8 @@ public class CustomAttributesList implements Serializable, MageventoryConstants 
                 .get(MAGEKEY_ATTRIBUTE_CONFIGURABLE)) == 1);
         customAttr.setUseForSearch(JobCacheManager.safeParseInt(map
                 .get(MAGEKEY_ATTRIBUTE_USE_FOR_SEARCH)) == 1);
-        // TODO remove default 1 value for safeParseInt call whent API will be
-        // fixed
-        customAttr.setCopyFromSearch(JobCacheManager.safeParseInt(
-                map.get(MAGEKEY_ATTRIBUTE_COPY_FROM_SEARCH), 1) == 1);
+        customAttr.setCopyFromSearch(JobCacheManager.safeParseInt(map
+                .get(MAGEKEY_ATTRIBUTE_COPY_FROM_SEARCH)) == 1);
         customAttr.setOptionsFromServerResponse(JobCacheManager
                 .getObjectArrayFromDeserializedItem(map.get(MAGEKEY_ATTRIBUTE_OPTIONS)));
         List<CustomAttributeOption> options = customAttr.getOptions();
