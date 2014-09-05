419abdbd900759a1ba9a58f85063ae3c0f92004e
Andrew Gilman <a.gilman@massey.ac.nz>
2014-09-05 14:39:54 +1200

    Added check to show attr code instead of label in Copy_to menu if label is empty.

--- mvApp/src/com/mageventory/activity/WebActivity.java
+++ mvApp/src/com/mageventory/activity/WebActivity.java
@@ -1438,7 +1438,10 @@ public class WebActivity extends BaseFragmentActivity implements MageventoryCons
             if (mTextAttributes != null) {
                 // add menu item for each custom text attribute to the menu
                 for (final CustomAttributeSimple attribute : mTextAttributes) {
-                    MenuItem mi = menu.add(attribute.getMainLabel());
+                    String menuLabel = attribute.getMainLabel();
+                    if (menuLabel==null) // so that attrs with no label don't appear as empty menu items
+                        menuLabel = attribute.getCode();
+                    MenuItem mi = menu.add(menuLabel);
                     mi.setOnMenuItemClickListener(new OnMenuItemClickListener() {
         
                         @Override
